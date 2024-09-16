5b5a932ec75794d40e48e27ab9ff0ceae342963e
adem-fennani <adem.fennani@esprit.tn>
2024-09-16 23:34:17 +0100

    Prosit 0

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ZooManagement z = new ZooManagement();
+
+        System.out.print(z.zooName + " se compose de " + z.nbrecages + "cages.");
+    }
+}
---
+++ src/ZooManagement.java
@@ -0,0 +1,4 @@
+public class ZooManagement {
+    int nbrecages = 20;
+    String zooName = "My Zoo";
+}
